03b895b9d6165168392457a816c02e64d07e1b26
Rodrigo Rosa <rodrigo.sr6@gmail.com>
2022-04-03 09:27:59 -0300

    deploy 10

--- src/handler.ts
+++ src/handler.ts
@@ -6,11 +6,19 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.get("/", async (req, res) => {
-  const message = await prisma.message.findMany();
-  return res.json({
-    text: "Hello World",
-    message,
-  });
+  try {
+    const message = await prisma.message.findMany();
+    return res.json({
+      text: "Hello World",
+      message,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      database_url: process.env.DATABASE_URL,
+      error,
+    });
+  }
 });
 
 export const server = serverless(app);
